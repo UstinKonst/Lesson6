ca41bb4d3c679e490c3234ce444a16b19bfb2086
UstinKonst <ustinovskuk@mail.ru>
2020-06-03 23:40:59 +0300

    Initial commit

---
+++ src/Client1/Server.java
@@ -0,0 +1,56 @@
+package Client1;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Server {
+
+
+    public static void main(String[] args) {
+        Server server = new Server();
+    }
+
+    public Server() {
+        ServerSocket server = null;
+        Socket socket = null;
+
+        try {
+            server = new ServerSocket(3443);
+            System.out.println("Сервер работает");
+            socket = server.accept();
+            System.out.println("Клиент подключился");
+            Scanner in = new Scanner(socket.getInputStream());
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            Scanner sc = new Scanner(System.in);
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (true) {
+                        String msg = sc.nextLine();
+                        out.println(msg);
+                    }
+                }
+            }).start();
+
+            while (true) {
+                String msg = in.nextLine();
+                if (msg.equals("/end")) break;
+                System.out.println("Client: " + msg);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                socket.close();
+                server.close();
+                System.out.println("Сервер закрыт");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
